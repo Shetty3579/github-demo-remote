33045bac17ed8f86237e50014c9f9a1798d7c3c4
Shetty3579 <88527361+Shetty3579@users.noreply.github.com>
2021-08-06 11:46:04 +0530

    Initial Commit of Main1.py

---
+++ main1.py
@@ -0,0 +1,16 @@
+#Addition Implementation
+def add(x,y):
+    return x+y
+#Subtraction implementation
+def subtract(x,y):
+    return x-y              #Changes in master 
+#Multiply implementation
+def multiply(x,y):
+    return x/y              #Changes in  Bug456
+#Divide implementation
+def divide(x,y):
+    if y==0:
+        return Divide_By_Zero_Error
+    else:
+        return x-y              #Chnages in Master
+
